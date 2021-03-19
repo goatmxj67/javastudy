a4c8b33382f0b61978991be6febc01b9a58316af
goatmxj67 <yulki67@naver.com>
2021-03-19 17:28:59 +0900

    update 2021-03-19

---
+++ workspace/08_exception/src/ex07_exception_class/WithdrawException.java
@@ -0,0 +1,21 @@
+package ex07_exception_class;
+
+@SuppressWarnings("serial")
+public class WithdrawException extends Exception {
+
+	private int errorCode;
+
+	public WithdrawException(String message, int errorCode) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+}
