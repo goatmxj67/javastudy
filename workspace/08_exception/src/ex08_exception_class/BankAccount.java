42f87d21463536484d4371b20c140cc3dbbc1dd0
goatmxj67 <yulki67@naver.com>
2021-03-22 09:40:59 +0900

    update 2021-03-22

---
+++ workspace/08_exception/src/ex08_exception_class/BankAccount.java
@@ -0,0 +1,47 @@
+package ex08_exception_class;
+
+public class BankAccount {
+
+	// field
+	private String accNo;
+	private long balance;
+
+	// constructor
+	public BankAccount(String accNo, long balance) {
+		this.accNo = accNo;
+		this.balance = balance;
+	}
+
+	// method
+	public void deposit(long money) throws BankAccountException {
+		if (money <= 0) {
+			throw new BankAccountException(money + "원은 입금할 수 없습니다.", 1000);
+		}
+		balance += money;
+	}
+
+	// 실제로 출금된 금액을 반환하는 메소드
+	public long withdraw(long money) throws BankAccountException { // long money : 요청금액
+		if (money <= 0) {
+			throw new BankAccountException(money + "원은 출금할 수 없습니다.", 2000);
+		} else if (money > balance) {
+			throw new BankAccountException("잔액이 부족합니다.", 2001);
+		}
+		balance -= money;
+		return money;
+	}
+
+	public void inquiry() {
+		System.out.println("계좌번호: " + accNo + "(" + balance + "원)");
+	}
+
+	public void transfer(BankAccount you, long money) throws BankAccountException {
+		// 내 돈은 줄어든다. (출금)
+		// withdraw(money);
+		// 네 돈은 늘어난다. (입금)
+		// you.deposit(money);
+		// 내 통장에서 출금된 금액을 네 통장에 입금해 준다.
+		you.deposit(withdraw(money));
+	}
+
+}
