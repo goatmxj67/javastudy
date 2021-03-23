ce9aed2b14349f977702a33bb631a39f56554c75
goatmxj67 <yulki67@naver.com>
2021-03-23 15:23:07 +0900

    update 2021-03-23

--- workspace/09_collection/src/ex08_map/LoopForHashMap.java
+++ workspace/09_collection/src/ex08_map/LoopForHashMap.java
@@ -22,12 +22,34 @@ public class LoopForHashMap {
 			System.out.println(key + ": " + me.get(key));
 		}
 
-		// 2. Iterator 인터페이스
+		// 2. Iterator 인터페이스 => 1회용(다시 만들어서 써야 함)
 		Iterator<String> itr = keys.iterator();
 
 		while (itr.hasNext()) {
 			String key = itr.next();
 			String value = me.get(key);
+			System.out.println(key + ": " + value);
+		}
+
+		// 3. 데이터 쌍(pair)으로 뺄 수 있습니다.
+		// entry : key와 value를 묶어서 entry라고 합니다.
+		// key : getKey()
+		// value: getValue()
+		Set<Map.Entry<String, String>> entrys = me.entrySet();
+
+		// 1) for
+		for (Map.Entry<String, String> entry : entrys) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			System.out.println(key + ": " + value);
+		}
+
+		// 2) Iterator
+		Iterator<Map.Entry<String, String>> itr2 = entrys.iterator();
+		while (itr2.hasNext()) {
+			Map.Entry<String, String> entry = itr2.next();
+			String key = entry.getKey();
+			String value = entry.getValue();
 			System.out.println(key + ": " + value);
 		}
 
