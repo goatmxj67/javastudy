c81462acde3cfa57472f71833fa7e5cfc6f2d966
goatmxj67 <yulki67@naver.com>
2021-03-25 12:16:45 +0900

    update 2021-03-25

---
+++ workspace/11_jdbc/src/dto/StaffDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+// DB Table에 저장된 행(Row)을 저장하는 클래스
+// VO : Value Object
+// DTO : Data Transfer Object
+
+// 테이블 칼럼   클래스 필드
+// BOARD_NO		 board_no 또는 boardNo
+// BNO			 bNo 또는 bno 
+
+// 디폴트 생성자 필수 : 생성자를 안 만들거나, 디폴트 생성자와 필드를 이용한 생성자를 모두 만듭니다.
+// getter/setter 필수
+
+@Data
+public class StaffDto {
+
+	private int no;
+	private String name;
+	private String department;
+	private Date hireDate;
+
+}
---
+++ workspace/11_jdbc/src/sql/staff.sql
@@ -0,0 +1,12 @@
+-- staff 테이블 삭제
+DROP TABLE staff;
+
+-- staff 테이블 추가
+CREATE TABLE staff
+(
+	no NUMBER,
+	name VARCHAR2(100),
+	department VARCHAR2(100),
+	hireDate DATE,
+	PRIMARY KEY(no)
+);
