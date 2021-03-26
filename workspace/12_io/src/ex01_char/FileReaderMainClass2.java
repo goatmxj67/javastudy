c30084b04b88db6276db26cad0f1f9dce80a89d7
goatmxj67 <yulki67@naver.com>
2021-03-26 11:27:11 +0900

    update 2021-03-26

---
+++ workspace/12_io/src/ex01_char/FileReaderMainClass2.java
@@ -0,0 +1,30 @@
+package ex01_char;
+
+import java.io.FileReader;
+
+public class FileReaderMainClass2 {
+
+	public static void main(String[] args) {
+
+		try (FileReader fr = new FileReader("text3.txt")) {
+
+			// char[] cbuf = new char[5]; // 안녕하세요
+									      // . \n반갑습
+									      // 니다. (갑습) -> 덮어쓰기 안됨
+
+			while (true) {
+				char[] cbuf = new char[5];
+				int readCharCount = fr.read(cbuf); // 읽은 내용은 cbuf에 저장되고, readCharCount에는 읽은 글자 수가 저장됩니다.
+												   // 파일의 끝에 도달하면 -1을 readCharCount에 반환합니다.
+				if (readCharCount == -1)
+					break;
+				System.out.print(cbuf);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
